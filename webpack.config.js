a8a0bcf7f72ab94c0e68d4007c9bf54327a2042c
Joel Tio <joeltiojinhon@gmail.com>
2018-01-27 18:21:46 +0800

    Add webpack and npm config

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var debug = process.env.NODE_ENV != "production";
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+    context: __dirname,
+    devtool: debug? "inline-sourcemap" : false,
+    entry: ["babel-polyfill", "./src/js/index.js"],
+    output: {
+        filename: "client.min.js",
+        path: path.resolve(__dirname, "build")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["react", "env"],
+                    plugins: [
+                        "react-html-attrs",
+                        ["react-css-modules", {"filetypes": {".scss": {"syntax": "postcss-scss"}}}]
+                    ]
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader?sourceMap',
+                    'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader?sourceMap',
+                    'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: debug ? [] : [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, soucemap: false })
+    ]
+};
